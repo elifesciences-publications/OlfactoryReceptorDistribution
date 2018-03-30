1e0e4c93832b81d746e5121ceec69001a821fb7a
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-03-30 15:56:37 -0400

    Added option for PNG images to safe_print.

--- graphing/safe_print.m
+++ graphing/safe_print.m
@@ -1,10 +1,41 @@
-function safe_print(fname)
-% safe_print Print image to PDF, avoiding overwrite.
+function safe_print(fname, type, varargin)
+% SAFE_PRINT Print image to file, avoiding overwrite.
+%   SAFE_PRINT(fname) saves the current figure to a PDF.
+%
+%   SAFE_PRINT(fname, 'png') saves the current figure to a PNG file. If no
+%   additional arguments are passed (see below), this includes the '-r300'
+%   option to use 300 DPI.
+%
+%   Additional arguments are passed to Matlab's PRINT command.
+%
+%   The extension is added automatically if not provided (which is
+%   recommended).
+
+if nargin < 2
+    type = 'pdf';
+end
+
+if strcmp(type, 'png') && isempty(varargin)
+    varargin = {'-r300'};
+end
+
+[~, ~, ext] = fileparts(fname);
+if isempty(ext)
+    fname = [fname '.' type];
+end
 
 if exist(fname, 'file')
     warning([mfilename ':fexist'], 'File already exists.');
     return;
 end
-print('-dpdf', fname);
+
+switch type
+    case 'pdf'
+        print('-dpdf', varargin{:}, fname);
+    case 'png'
+        print('-dpng', varargin{:}, fname);
+    otherwise
+        error([mfilename ':badtype'], 'Unrecognized file type.');
+end
 
 end
