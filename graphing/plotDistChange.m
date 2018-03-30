b162ecbe06772d4ec84c7dc66d20f4f651db6a0c
Tiberiu Tesileanu <ttesileanu@gmail.com>
2018-03-30 15:58:00 -0400

    Added options for horizontal bars and configurable label font and rotation for plotDistChange.

--- graphing/plotDistChange.m
+++ graphing/plotDistChange.m
@@ -13,9 +13,14 @@ function plotDistChange(K1, K2, varargin)
 %                on the right
 %        'bar':  a barplot showing the change K2 - K1 with different colors
 %                for increases vs. decreases in abundance
+%        'barh': like 'bar', but using horizontal bars instead
 %    'receptornames':
 %       Cell array of receptor names. For now this is only used with the
 %       'bar' option.
+%    'axfontsize':
+%       Font size to use for the axes.
+%    'labelrot':
+%       Angle by which to rotate tick labels.
 
 % parse optional arguments
 parser = inputParser;
@@ -24,8 +29,10 @@ parser.FunctionName = mfilename;
 
 parser.addParameter('beautifyopts', {}, @(c) isempty(c) || (iscell(c) && isvector(c)));
 parser.addParameter('method', 'plot', @(s) ischar(s) && isvector(s) && ...
-    ismember(s, {'plot', 'bar'}));
+    ismember(s, {'plot', 'bar', 'barh'}));
 parser.addParameter('receptornames', {}, @(c) isempty(c) || (iscell(c) && isvector(c)));
+parser.addParameter('axfontsize', 6, @(x) isnumeric(x) && isscalar(x) && x > 0);
+parser.addParameter('labelrot', 60, @(x) isnumeric(x) && isscalar(x));
 
 % handle the 'defaults' option
 if nargin == 1 && strcmp(K1, 'defaults')
@@ -76,28 +83,54 @@ switch params.method
         ax.TickLength = [0 0];
         ax.XTick = [0 1];
         ax.XTickLabel = {'env1', 'env2'};
-    case 'bar'
+    case {'bar', 'barh'}
         diffK = K1 - K2;
-        bar(diffK.*(diffK > 0), 0.8, 'facecolor', col1, 'edgecolor', 'none');
+        
+        switch params.method
+            case 'bar'
+                barfct = @bar;
+            case 'barh'
+                barfct = @barh;
+        end
+        
+        barfct(diffK.*(diffK > 0), 0.8, 'facecolor', col1, 'edgecolor', 'none');
         hold on;
-        bar(diffK.*(diffK < 0), 0.8, 'facecolor', col2, 'edgecolor', 'none');
+        barfct(diffK.*(diffK < 0), 0.8, 'facecolor', col2, 'edgecolor', 'none');
         
         crt_yl = max(abs(diffK));
-        xlim([0 M+1]);
-        ylim([-crt_yl crt_yl]);
+        switch params.method
+            case 'bar'
+                xlim([0 M+1]);
+                ylim([-crt_yl crt_yl]);
+            case 'barh'
+                ylim([0 M+1]);
+                xlim([-crt_yl crt_yl]);
+        end
         
         beautifygraph(params.beautifyopts{:});
         
         ax = gca;
-        ax.YTick = [];
-        ax.XTick = 1:M;
-        if ~isempty(params.receptornames)
-            ax.XTickLabel = params.receptornames;
+        switch params.method
+            case 'bar'
+                ax.YTick = [];
+                ax.XTick = 1:M;
+                if ~isempty(params.receptornames)
+                    ax.XTickLabel = params.receptornames;
+                end
+                ax.XMinorTick = 'off';
+                
+                xtickangle(params.labelrot);
+            case 'barh'
+                ax.XTick = [];
+                ax.YTick = 1:M;
+                if ~isempty(params.receptornames)
+                    ax.YTickLabel = params.receptornames;
+                end
+                ax.YMinorTick = 'off';
+                
+                ytickangle(params.labelrot);
         end
-        ax.XMinorTick = 'off';
-        ax.FontSize = 6;
-        
-        xtickangle(60);        
+        ax.FontSize = params.axfontsize;
 end
 
 end
