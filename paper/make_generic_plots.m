3b03718120d7763ee0e59e2017582bcaf46ceadf
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2018-12-14 15:14:24 -0600

    Add options for other sensing matrices

--- paper/make_generic_plots.m
+++ paper/make_generic_plots.m
@@ -1,9 +1,130 @@
 % Make plots meant to show generic properties of the optimization.
+%
+% This script's behavior can be modified by defining some variables before
+% running. When these variables are not defined, default values are used.
+%
+% Options:
+%   S_choice:
+%       Choice of sensing matrix to use. Options are:
+%         'fly'     -- Drosophila sensing matrix from Hallem&Carlson (2006)
+%         'mouse'   -- mouse sensing matrix from Saito et al. (2009)
+%         'fly_scrambled'
+%                   -- fly sensing matrix randomly scrambled across both
+%                      rows and columns
+%         'mouse_scrambled'
+%                   -- mouse sensing matrix randomly scrambled across both
+%                      rows and columns
+%         'artificial'
+%                   -- an artificial sensing matrix using a range of tuning
+%                      widths
+%         'artificial_narrow'
+%                   -- artificial sensing matrix with narrowly-tuned
+%                      receptors
+%         'artificial_wide'
+%                   -- artificial sensing matrix with broadly-tuned
+%                      receptors
+%         'gaussian'-- artificial sensing matrix with entries drawn from a
+%                      zero-centered Gaussian
+%         'binary'  -- artificial sensing matrix with binary 0 or 1 entries
+%         'signed'  -- artificial sensing matrix with entries that are
+%                      either 0 or +1/-1
+%   S_size:
+%       Size of sensing matrix, [n_receptors, n_odorants]. This is usually
+%       [24, 110] (the size of the Drosophila matrix), unless `S_choice` is
+%       'mouse' or 'mouse_scrambled'.
+%   artif_tuning:
+%       Range of tuning width used with `generate_random_sensing` when
+%       `S_choice` is one of the artificial settings. Only the first (last)
+%       element of `artif_tuning` is used for 'artificial_narrow'
+%       ('artifical_wide').
+%   artif_snr:
+%       SNR setting to use with `generate_random_sensing` when `S_choice`
+%       is one of the artificial settings.
 
-%% Load Hallem&Carlson sensing data
+%% Setup
+
+setdefault('S_choice', 'fly');
+setdefault('S_size', [24, 110]);
+setdefault('artif_tuning', [0.2 0.8]);
+setdefault('artif_snr', 200);
+setdefault('Ktot', 100);
+setdefault('n_samples', 100);
+setdefault('n_samples_frac', 10);
+setdefault('receptor_fractions', []);
+
+%% Preprocess some options
+
+if ~strcmp(S_choice, 'fly')
+    postfix = ['_' S_choice];
+else
+    postfix = '';
+end
+
+%% Setup some non-configurable settings
+
+optim_args = {'optimopts', ...
+        {'MaxFunctionEvaluations', 50000, 'Display', 'notify-detailed'}, ...
+    'method', 'lagsearch', 'sumtol', 1e-3};
+
+% generate colormap for covariance matrix plots
+% cmap_covmat = divergent([0.21 0.17 0.53], [0.98 0.40 0.17], 256);
+cmap_covmat = divergent([0.177 0.459 0.733], [0.737 0.180 0.172], 64, [0.969 0.957 0.961]);
+
+%% Load fly (Hallem&Carlson) sensing data
 
 sensing_fly = open('data/flyResponsesWithNames.mat');
 S_fly = sensing_fly.relRates';
+
+% normalize by standard deviations of background rates
+S_fly_normalized = bsxfun(@rdivide, S_fly, sensing_fly.bkgStd');
+
+%% Load mouse (Saito et al.) sensing matrix
+
+% load response curves
+mouse_responses = open(fullfile('data', 'mouse_receptor_curves.mat'));
+S_mouse = mouse_responses.sensing_matrix(:, :, 11);
+% set missing data to 0
+S_mouse(isnan(S_mouse)) = 0;
+% remove receptors that don't respond to anything
+mask = ~all(S_mouse == 0, 2);
+S_mouse = S_mouse(mask, :);
+
+%% Select a sensing matrix
+
+% keep random stuff reproducible
+rng(8237);
+
+switch S_choice
+    case 'fly'
+        S = S_fly_normalized;
+    case 'mouse'
+        % adding a bit of random noise to remove degeneracies from the very
+        % sparse S_mouse (rank(S_mouse) = 50 while size(S_mouse) = [59, 63]).
+        S = 50*(S_mouse + 0.01*randn(size(S_mouse)));
+    case 'fly_scrambled'
+        S = 0.1*S_fly_normalized;
+        S(:) = S(randperm(numel(S)));
+    case 'mouse_scrambled'
+        S = 50*S_mouse;
+        S(:) = S(randperm(numel(S)));
+    case 'artificial'
+        S = 100*generate_random_sensing(S_size(1), S_size(2), artif_tuning, artif_snr);
+    case 'artificial_narrow'
+        S = 100*generate_random_sensing(S_size(1), S_size(2), artif_tuning(1), artif_snr);
+    case 'artificial_wide'
+        S = 100*generate_random_sensing(S_size(1), S_size(2), artif_tuning(2), artif_snr);
+    case 'gaussian'
+        S = 2*randn(S_size);
+    case 'binary'
+        S = 5*(rand(S_size) > 0.7);
+    case 'signed'
+        S = 5*sign(rand(S_size) - 0.5);
+        S = S .* (rand(S_size) > 0.7);
+    otherwise
+        error('Unrecognized setting for S_choice.');
+end
+
+S_size = size(S);
 
 %% Plot a sample of the Hallem&Carlson sensing data
 
@@ -24,9 +145,9 @@ ax.Position = [0 0 1 1];
 % cmap = divergent([0.177 0.459 0.733], [0.737 0.180 0.172], 64);
 cmap = divergent([0.177 0.459 0.733], [0.737 0.180 0.172], 64, [0.969 0.957 0.961]);
 
-cl = quantile(flatten(S_fly(or_idxs, odor_idxs)), 0.9);
+cl = quantile(flatten(S_fly_normalized(or_idxs, odor_idxs)), 0.9);
 
-plotheat(S_fly(or_idxs, odor_idxs), [-cl cl]);
+plotheat(S_fly_normalized(or_idxs, odor_idxs), [-cl cl]);
 colormap(cmap);
 
 axis off;
@@ -45,7 +166,7 @@ rng(92376);
 % * the actual variances of odor concentrations in natural scenes are
 %   unknown, so we effectively fit them to obtain neuron numbers in the
 %   right ballpark
-Gamma = generate_environment('rnd_diag_const', size(S_fly, 2), ...
+Gamma = generate_environment('rnd_diag_const', size(S, 2), ...
     'diag_mu', 1e-6, 'diag_size', 1e-6, 'offdiag_mu', 1e-7);
 
 %% Get distribution at various values for Ktot
@@ -53,13 +174,9 @@ Gamma = generate_environment('rnd_diag_const', size(S_fly, 2), ...
 % Ktot_values = [0 logspace(log10(0.1), log10(15000), 100)];
 Ktot_values = [0 logspace(log10(5), log10(7.5e5), 100)];
 
-% normalize by standard deviations of background rates
-S_fly_normalized = bsxfun(@rdivide, S_fly, sensing_fly.bkgStd');
+[K, info_values, Q, info_fct] = calculate_optimal_dist(S, Gamma, Ktot_values);
 
-[K, info_values, Q, info_fct] = calculate_optimal_dist(...
-    S_fly_normalized, Gamma, Ktot_values);
-
-[M, N] = size(S_fly);
+[M, N] = size(S);
 
 % find where each receptor kicks in
 minK_by_type = ones(M, 1);
@@ -112,13 +229,15 @@ xlh.Units = 'characters';
 xlh.Position(2) = xlh.Position(2) - 0.15;
 ylabel('OSNs by receptor type');
 
-% find out which receptors we've displayed, and in what order they appeared
-% so we can create a legend
-rec_dispd = rec_order(minK_ordered <= idx_max);
-h_leg = legend(h_area(rec_dispd), sensing_fly.orNames(rec_dispd), 'location', 'northwest', ...
-    'fontsize', 6);
-h_leg.Position = [h_leg.Position(1) - 0.005 h_leg.Position(2) + 0.09 h_leg.Position(3:4)];
-legend('boxoff');
+if strcmp(S_choice, 'fly')
+    % find out which receptors we've displayed, and in what order they appeared
+    % so we can create a legend
+    rec_dispd = rec_order(minK_ordered <= idx_max);
+    h_leg = legend(h_area(rec_dispd), sensing_fly.orNames(rec_dispd), 'location', 'northwest', ...
+        'fontsize', 6);
+    h_leg.Position = [h_leg.Position(1) - 0.005 h_leg.Position(2) + 0.09 h_leg.Position(3:4)];
+    legend('boxoff');
+end
 beautifygraph(ax1, 'fontscale', 0.667, 'ticksize', 12);
 
 ax1.XMinorTick = 'off';
@@ -162,10 +281,11 @@ ax2.LineWidth = 0.5;
 ax3 = axes;
 ax3.Units = 'normalized';
 ax3.OuterPosition = [2/3 0 1/3 1];
-rectype_count = zeros(size(Ktot_values));
-for i = 1:length(minK_ordered)
-    rectype_count(minK_ordered(i):end) = rectype_count(minK_ordered(i):end) + 1;
-end
+% rectype_count = zeros(size(Ktot_values));
+% for i = 1:length(minK_ordered)
+%     rectype_count(minK_ordered(i):end) = rectype_count(minK_ordered(i):end) + 1;
+% end
+rectype_count = sum(K_nz, 1);
 semilogx(Ktot_values, rectype_count, 'linewidth', 1);
 xlabel('Number of OSNs');
 ylabel('Active receptor types');
@@ -194,4 +314,6 @@ ax1.Position(4) = hmin;
 ax2.Position(4) = hmin;
 ax3.Position(4) = hmin;
 
-safeprint(fullfile('figs', 'dependence_on_Ktot'));
+if isempty(postfix)
+    safeprint(fullfile('figs', ['dependence_on_Ktot' postfix]));
+end
