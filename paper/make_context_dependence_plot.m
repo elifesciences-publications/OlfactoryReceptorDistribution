0820ddf557c37650c383c5bf2957d2ea9a394061
Tiberiu Tesileanu <ttesileanu@flatironinstitute.org>
2019-01-04 11:44:01 -0600

    Add shading to show mis-/matches in the sign of abundance changes

--- paper/make_context_dependence_plot.m
+++ paper/make_context_dependence_plot.m
@@ -15,6 +15,24 @@ fig.Position(3:4) = [2.4 2];
 
 diff1 = results.K1_pert - results.K1;
 diff2 = results.K2_pert - results.K2;
+
+% emphasize regions that have the same sign, and regions with opposite sign
+max1 = 1.10*max([diff1(:) ; diff2(:)]);
+min1 = 1.10*min([diff1(:) ; diff2(:)]);
+
+max2 = max1;
+min2 = min1;
+
+hold on;
+fill([0, max1, max1, 0, 0], [0, 0, max2, max2, 0], hex2color('EBF2F8'), ...
+    'edgecolor', 'none');
+fill([0, min1, min1, 0, 0], [0, 0, min2, min2, 0], hex2color('EBF2F8'), ...
+    'edgecolor', 'none');
+fill([0, max1, max1, 0, 0], [0, 0, min2, min2, 0], hex2color('FAF0F0'), ...
+    'edgecolor', 'none');
+fill([0, min1, min1, 0, 0], [0, 0, max2, max2, 0], hex2color('FAF0F0'), ...
+    'edgecolor', 'none');
+
 smartscatter(diff1(:), diff2(:), 'size', 10, 'alpha', 1);
 % smartscatter(diff1(:), diff2(:), 'size', 5, 'color', hex2color('BC2E2C'), ...
 %     'alpha', 0.2);
@@ -26,6 +44,8 @@ ylabel('\DeltaK_2');
 % ylim([-data_lim, data_lim]);
 
 axis equal;
+
+xlim([min1 max1]);
 
 colormap(cmap);
 % colorbar;
@@ -55,11 +75,34 @@ sub_mask = (abs(diff1(:)) < klim) & (abs(diff2(:)) < klim);
 sub_diff1 = diff1(sub_mask);
 sub_diff2 = diff2(sub_mask);
 
+% emphasize regions that have the same sign, and regions with opposite sign
+sub_max1 = 1.10*max([sub_diff1(:) ; sub_diff2(:)]);
+sub_min1 = 1.10*min([sub_diff1(:) ; sub_diff2(:)]);
+
+sub_max2 = sub_max1;
+sub_min2 = sub_min1;
+
+hold on;
+fill([0, sub_max1, sub_max1, 0, 0], ...
+     [0, 0, sub_max2, sub_max2, 0], hex2color('EBF2F8'), ...
+     'edgecolor', 'none');
+fill([0, sub_min1, sub_min1, 0, 0], ...
+     [0, 0, sub_min2, sub_min2, 0], hex2color('EBF2F8'), ...
+     'edgecolor', 'none');
+fill([0, sub_max1, sub_max1, 0, 0], ...
+     [0, 0, sub_min2, sub_min2, 0], hex2color('FAF0F0'), ...
+     'edgecolor', 'none');
+fill([0, sub_min1, sub_min1, 0, 0], ...
+     [0, 0, sub_max2, sub_max2, 0], hex2color('FAF0F0'), ...
+     'edgecolor', 'none');
+
 smartscatter(sub_diff1, sub_diff2, 'size', 10, 'alpha', 1);
 xlabel('\DeltaK_1');
 ylabel('\DeltaK_2');
 
 axis equal;
+
+xlim([sub_min1 sub_max1]);
 
 % xlim([-0.0019 0.0019]);
 % ylim([-0.0019 0.0019]);
